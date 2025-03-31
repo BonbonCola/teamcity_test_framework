836719f77526431bb0ae69a30f8839db8a3b5f2e
Olga Ziuziakina <flyin.zeppelin@gmail.com>
2025-03-31 11:28:51 +0100

    CI, дебаг - добавила скриншот, url и healthcheck, тк запусть теста для настройки падает

--- main/ui/first_start_page.py
+++ main/ui/first_start_page.py
@@ -1,6 +1,3 @@
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from main.api.configs.config import Config
 from selenium.webdriver.common.by import By
 
