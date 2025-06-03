5b0f0b35215ee6a735ec407f95f5ec11ec21248f
Olga Ziuziakina <flyin.zeppelin@gmail.com>
2025-06-03 14:56:42 +0100

    добавлен logger для селениум действий

--- main/utils/logger.py
+++ main/utils/logger.py
@@ -3,9 +3,13 @@ import logging
 # Настройка логирования
 def configure_logging():
     logging.basicConfig(
-        level=logging.DEBUG, # минимальный уровень для root-логгера
-        format="%(asctime)s – %(name)s – %(levelname)s – %(message)s",
+        level=logging.INFO, # минимальный уровень для root-логгера
+        format="%(asctime)s – %(name)s – %(levelname)s – %(message)s", #Формат: время – имя логгера – уровень – сообщение
         handlers=[
             logging.StreamHandler(),  # Вывод в консоль
         ]
     )
+    #«Заглушаем» шум от ненужных модулей:
+    #logging.getLogger("selenium").setLevel(logging.INFO)  # пример: selenium меньше подробностей
+    logging.getLogger("faker.factory").setLevel(logging.WARNING)  # отключаем шум от faker.factory
+    logging.getLogger("faker").setLevel(logging.WARNING)
